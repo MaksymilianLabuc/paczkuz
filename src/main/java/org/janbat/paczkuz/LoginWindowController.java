35a3695f1a02191a3312f5c62fc0a93950b561b0
maks <makslabuc@wp.pl>
2024-03-25 23:27:12 +0100

    kontrolery okien logowania i dodawania zlecen

--- src/main/java/org/janbat/paczkuz/LoginWindowController.java
+++ src/main/java/org/janbat/paczkuz/LoginWindowController.java
@@ -1,7 +1,14 @@
 package org.janbat.paczkuz;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginWindowController {
     @FXML
@@ -13,4 +20,12 @@ public class LoginWindowController {
     }
     @FXML
     protected void onLoginButtonClick(){System.out.println("Login");}
+    @FXML
+    public void switchToZlecenieWindow(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("zlecenieWindow.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/org/janbat/paczkuz/ZlecenieWindowController.java
+++ src/main/java/org/janbat/paczkuz/ZlecenieWindowController.java
@@ -1,4 +1,42 @@
 package org.janbat.paczkuz;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ZlecenieWindowController {
+    @FXML
+    MenuButton typTrasy;
+    @FXML
+    TextField Cel;
+    @FXML
+    private Parent root;
+    @FXML
+    public void initialize(){
+        typTrasy.setText("Trasy");
+        MenuItem m1 = new MenuItem("Trasa krajowa");
+        typTrasy.getItems().add(m1);
+    }
+    @FXML
+    public void switchToLoginWindow(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Stage stage = (Stage) root.getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void menuClick(ActionEvent event){
+        typTrasy.setText("test");
+        System.out.println("HERE");
+    }
 }
