d0fa1a9375137d9b52465066ac1c7b8acbff8e18
JO Krul <109252@g.elearn.uz.zgora.pl>
2024-05-22 00:01:03 +0200

    panel admina -> implementacja

--- src/main/java/org/janbat/paczkuz/HelloApplication.java
+++ src/main/java/org/janbat/paczkuz/HelloApplication.java
@@ -5,17 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        /*File cssFile = new File("src/main/resources/dark-mode.css");
+        scene.getStylesheets().add(cssFile.toURI().toString());*/ //zmiana na tryb ciemny
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+
     }
 
     public static void main(String[] args) {
