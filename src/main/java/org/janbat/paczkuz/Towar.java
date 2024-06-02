43737c6be8fc46cd3c4e8b1cac1dc655e0486588
xJackiee <kuba.m0209@gmail.com>
2024-06-02 21:37:59 +0200

    dodanie komentarzy

--- src/main/java/org/janbat/paczkuz/Towar.java
+++ src/main/java/org/janbat/paczkuz/Towar.java
@@ -5,32 +5,67 @@ public class Towar {
     public int ciezar;
     public int ilosc;
 
+    /**
+     * Konstruktor tworzący obiekt klasy Towar.
+     * @param n nazwa towaru.
+     * @param c ciężar towaru.
+     * @param i ilość towaru.
+     */
     public Towar(String n, int c, int i){
         nazwa = n;
         ciezar = c;
         ilosc = i;
     }
+
+    /**
+     * Domyślny konstruktor tworzący pusty obiekt klasy Towar.
+     */
     public Towar(){}
+
+    /**
+     * Zwraca nazwę towaru.
+     * @return nazwa towaru.
+     */
     public String getNazwa() {
         return nazwa;
     }
 
+    /**
+     * Ustawia nazwę towaru.
+     * @param nazwa nowa nazwa towaru.
+     */
     public void setNazwa(String nazwa) {
         this.nazwa = nazwa;
     }
 
+    /**
+     * Zwraca ciężar towaru.
+     * @return ciężar towaru.
+     */
     public int getCiezar() {
         return ciezar;
     }
 
+    /**
+     * Ustawia ciężar towaru.
+     * @param ciezar nowy ciężar towaru.
+     */
     public void setCiezar(int ciezar) {
         this.ciezar = ciezar;
     }
 
+    /**
+     * Zwraca ilość towaru.
+     * @return ilość towaru.
+     */
     public int getIlosc() {
         return ilosc;
     }
 
+    /**
+     * Ustawia ilość towaru.
+     * @param ilosc nowa ilość towaru.
+     */
     public void setIlosc(int ilosc) {
         this.ilosc = ilosc;
     }
--- src/main/java/org/janbat/paczkuz/Ustawienia.java
+++ src/main/java/org/janbat/paczkuz/Ustawienia.java
@@ -13,23 +13,44 @@ public class Ustawienia {
     }}; //Lista języków
     private static String jezyk = "Polski";
 
+    /**
+     * Zwraca bieżący motyw aplikacji.
+     * @return nazwa motywu.
+     */
     public static String getMotyw() {
         return motyw;
     }
 
+    /**
+     * Ustawia motyw aplikacji.
+     * @param motyw nowy motyw.
+     */
     public static void setMotyw(String motyw) {
         Ustawienia.motyw = motyw;
     }
 
+    /**
+     * Zwraca bieżący język aplikacji.
+     * @return nazwa języka.
+     */
     public static String getJezyk() {
         return jezyk;
     }
 
+    /**
+     * Ustawia język aplikacji.
+     * Aktualizuje także ResourceBundle na podstawie wybranego języka.
+     * @param jezyk nowy język.
+     */
     public static void setJezyk(String jezyk) {
         Ustawienia.jezyk = jezyk;
         HelloApplication.paczkaJezykowa = ResourceBundle.getBundle("org.janbat.paczkuz.language", Ustawienia.getJezyki().get(jezyk));
     }
 
+    /**
+     * Zwraca mapę dostępnych języków wraz z ich lokalizacjami.
+     * @return mapa języków.
+     */
     public static HashMap<String, Locale> getJezyki() {
         return jezyki;
     }
